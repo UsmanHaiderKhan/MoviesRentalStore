ef3df2da4f02fd0982b93cc2373c19c0a06071dc
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-11-26 23:54:16 +0500

    Update added NEw Controller for Movies

---
+++ MoviesRentalStore/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using MoviesRentalStore.Models;
+using System.Web.Mvc;
+
+namespace MoviesRentalStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movies = new Movie()
+            {
+                Name = "Dome 4",
+
+            };
+            return View();
+        }
+    }
+}
