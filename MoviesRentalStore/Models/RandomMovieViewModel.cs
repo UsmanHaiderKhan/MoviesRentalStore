7ebfcff43e3743b13bf9036e9cc4851fad08b23b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-09 21:07:15 +0500

    Udpate : Added New Class ViewModel

---
+++ MoviesRentalStore/Models/RandomMovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MoviesRentalStore.Models
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customer { get; set; }
+    }
+}
