1e0e752d00bfa2c7f32f8ad9182be8da7ad0b75b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-09 21:04:06 +0500

    Update : SetUp Custome Routing Path  🚓 🚓

--- MoviesRentalStore/App_Start/RouteConfig.cs
+++ MoviesRentalStore/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MoviesRentalStore
@@ -12,7 +8,13 @@ namespace MoviesRentalStore
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
+            //routes.MapRoute(
+            //    "MoviesByRelaseDate",
+            //    "Movies/RelaseByDate/{year}/{month}",
+            //    new { controller = "Movies", action = "RelaseByDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" }
+            //);
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
