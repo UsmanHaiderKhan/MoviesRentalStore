513aabe5eb698a05c064d4d8ec7a963d2c79ec3d
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-09 21:04:54 +0500

    Update : Added New 👥 Customer controller

---
+++ MoviesRentalStore/Controllers/CustomersController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace MoviesRentalStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CustomerDetails(int id)
+        {
+            return View();
+        }
+    }
+}
