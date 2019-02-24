ab1de9ea9b946a2ef6ea8e8944862c9f7980d92a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-24 09:11:25 +0500

    Update Added New Classes in the project

---
+++ MoviesRentalStore/Models/MemberShipType.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesRentalStore.Models
+{
+    public class MemberShipType
+    {
+        public int Id { get; set; }
+        public short SignUpfee { get; set; }
+        public byte DurationinMonth { get; set; }
+        public byte DiscountRate { get; set; }
+
+    }
+}
