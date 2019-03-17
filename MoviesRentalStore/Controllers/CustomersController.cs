1c22d096719d4928bb2f4ff934408af045be87c8
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-03-17 09:48:00 +0500

    Update the Customer Controller

--- MoviesRentalStore/Controllers/CustomersController.cs
+++ MoviesRentalStore/Controllers/CustomersController.cs
@@ -1,13 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using MoviesRentalStore.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace MoviesRentalStore.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            return View();
+            var customer = _context.Customers.ToList();
+            return View(customer);
         }
 
         public ActionResult CustomerDetails(int id)
