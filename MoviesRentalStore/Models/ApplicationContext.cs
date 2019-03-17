3e04b419b80292532b8a0d3ef213e5d6409a9f2b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-03-17 09:48:31 +0500

    Udpate Added New Application Context File in the project

---
+++ MoviesRentalStore/Models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace MoviesRentalStore.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("constr")
+        {
+
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MemberShipType> MemberShipTypes { get; set; }
+
+
+    }
+}
